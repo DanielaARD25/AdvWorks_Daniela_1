55555298efaed5599b84d1a13d729e693cb7ee59
Daniela Rodriguez <104514305+DanielaARD25@users.noreply.github.com>
2025-03-14 20:53:50 -0600

    Creación del modelo y de la clase AppDbContext

---
+++ AdvWorks_Daniela_1/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AdvWorks_Daniela_1.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+    }
+}
---
+++ AdvWorks_Daniela_1/Models/SalesTerritory.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AdvWorks_Daniela_1.Models
+{
+    public class SalesTerritory
+    {
+        [Key]
+        public required int TerritoryID { get; set; }
+        public required string Name { get; set; }
+        public required string CountryRegionCode { get; set; }
+        public required string Group { get; set; }
+        public required decimal SalesYTD { get; set; }
+        public required decimal CostYTD { get; set; }
+        public required decimal CostLastYear { get; set; }
+        public required Guid rowguid { get; set; }
+        public required DateTime ModifiedDate { get; set; }
+
+
+        // Si es un valor nulo, es ejemplo: public string Name {get; set;} = string.Empty;
+    }
+}
